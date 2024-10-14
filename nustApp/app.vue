045c03b4901b306281ab99c2a4cf1a50fe95fbbb
aaa <922156@gmail.com>
2024-10-14 16:30:54 +0200

    first commit

---
+++ nustApp/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    try {
+      const users = await $axios.$get('/User'); // Fetch from /users endpoint
+      return { users };
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      return { users: [] };
+    }
+  }
+}
+</script>
---
+++ src/Entity/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $lastName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
