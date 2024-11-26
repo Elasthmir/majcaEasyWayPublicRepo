ecfda16752f16aaf7c9bd0eaa11623154114de4c
aaa <922156@gmail.com>
2024-11-26 17:19:00 +0100

    Losowanie odpowiedzi

--- assets/vue/controllers/Quizz.vue
+++ assets/vue/controllers/Quizz.vue
@@ -15,6 +15,14 @@ export default {
       type: Array,
       required: true,
     },
+    answerbadfirst: {
+      type: Array,
+      required: true,
+    },
+    answerbadsecond: {
+      type: Array,
+      required: true,
+    },
     topic: {
       type: String,
       required: true,
@@ -27,10 +35,10 @@ export default {
   data() {
     return {
       countDownResponse: this.records,
-      countDownResponseTimer: 1,
-      answerBad1: this.answerBad,
-      answers: ["-71", "50"],
-      
+      countdown: 30,
+      goodAnswersCounter: 0,
+      countDownResponseTimer: 4,
+      shuffledAnswers: [], // Holds the shuffled answers for the current question
       awesome: false,
       right: 0,
     };
@@ -42,80 +50,102 @@ export default {
   },
   methods: {
     toggleQuestion() {
-
       if (this.countDownResponseTimer > 0) {
         this.countDownResponseTimer -= 1;
       } else {
-        this.countDownResponseTimer = this.countDownResponse.length - 1;
+        this.countDownResponseTimer = -1;
       }
-
-      this.randomList(this.answers)
-      //this.randomList(this.countDownResponse)
-      this.right = 0
-      console.log(this.answer);
-      console.log("++++++++++");
-      console.log(this.answerbad[this.countDownResponseTimer].answerBad);
+      console.log(this.countDownResponseTimer);
+      this.shuffleAnswersForCurrentQuestion(); // Shuffle answers when toggling questions
+      this.right = 0;
     },
+    shuffleAnswersForCurrentQuestion() {
+      // Combine correct and incorrect answers for the current question
+      const answersForCurrentQuestion = [
+        this.answer[this.countDownResponseTimer].answer,
+        this.answerbad[this.countDownResponseTimer].answerBad,
+        this.answerbadfirst[this.countDownResponseTimer].answerBad,
+        this.answerbadsecond[this.countDownResponseTimer].answerBad,
+      ];
 
+      // Shuffle the combined answers
+      this.shuffledAnswers = this.shuffleArray(answersForCurrentQuestion);
+    },
+    shuffleArray(array) {
+      // Shuffle the array using Fisher-Yates shuffle
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
+    function () {
+      this.counting = true;
+    },
+    checkAnswer(answer) {
+      console.log("Selected answer:", answer);
+      const correctAnswer = this.answer[this.countDownResponseTimer].answer;
+      this.right = answer === correctAnswer ? 1 : 2;
+      this.right = answer === correctAnswer ? this.goodAnswersCounter++ : this.goodAnswersCounter;
+    },
     recordContent(record) {
-
       return record.question;
     },
     countDownTimer() {
       // Your countdown logic
     },
-    randomList(rand) {
-      rand.sort(() => 0.5 - Math.random());
-    },
-    checkAnswer(answer) {
-      console.log("pogo" + answer)
-      console.log(this.answers[0])
-      this.right = answer === this.answerbad[this.countDownResponseTimer].answerBad ? 1 : 2;
-    },
   },
   created() {
-    console.log("Component created"); // Ensure Vue instance is created
-    this.countDownResponse = this.records.slice(); // Initialize the array
+    console.log("Component created");
+    this.countDownResponse = this.records.slice();
+    this.shuffleAnswersForCurrentQuestion(); // Initialize with shuffled answers for the first question
     this.countDownTimer();
-    //this.randomList(this.countDownResponse);
-    //this.randomList(this.answers);
-
   },
   mounted() {
-
-    //console.log("Records:", this.records); // Check the passed props
+    const stopCountdown = setInterval(() => {
+      console.log('current countdown', this.countdown)
+      this.countdown -= 1
+      if (!this.countdown) clearInterval(stopCountdown)
+    }, 1000)
   },
+  
 };
 </script>
-
 <template>
   <div>
     <h1>{{ topic }}</h1>
     <p>Selected Mode: {{ selectedModeLabel }}</p>
     <ul v-if="records.length">
       <li v-for="(record, index) in records" :key="index">
-        <!-- Display the question content -->
         <div v-html="recordContent(record)"></div>
       </li>
     </ul>
     <p v-else>No questions available.</p>
+
+    <h1 v-if="countDownResponseTimer >= 0 && countDownResponseTimer < countDownResponse.length">
+      {{ countDownResponse[countDownResponseTimer].question }}
+    </h1>
+
+    <h1 v-if="countDownResponseTimer == -1">
+      nie ma
+      <a href="http://localhost:8000/user#">powrót</a>
+    </h1>
+    <!-- Render shuffled buttons -->
+    <div>
+      <button
+        v-for="(answer, index) in shuffledAnswers"
+        :key="index"
+        @click="checkAnswer(answer),toggleQuestion()"
+      >
+        {{ answer }}
+      </button>
+    </div>
+
+    <button @click="toggleQuestion()">Toggle</button>
+
+    <span v-if="right == 0"></span>
+    <span v-if="right == 1">Good</span>
+    <span v-if="right == 2">Bad</span>
+    <span>{{ goodAnswersCounter }}/{{ countDownResponse.length }} dobrych odpowiedzi</span>
   </div>
-
-  <button @click="toggleQuestion()">Toggle</button>
-  <br />
-  <button @click="checkAnswer(answers[1])">{{ this.answerbad[this.countDownResponseTimer].answerBad}}</button><br />
-  <button @click="checkAnswer(answers[0])">{{ answers[1] }}</button><br />
-  <button @click="checkAnswer(answers[2])">{{ answers[2] }}</button><br />
-  <button @click="checkAnswer(answers[3])">{{ answers[3] }}</button><br />
-  <h1 v-if="countDownResponseTimer >= 0 && countDownResponseTimer < countDownResponse.length">
-    {{ countDownResponse[countDownResponseTimer].question }}
-  </h1>
-  
-  <span v-if="right == 0"></span>
-  <span v-if="right == 1">good</span>
-  <span v-if="right == 2">bad</span>
 </template>
-
-<style>
-/* Include any component-specific styles here */
-</style>
